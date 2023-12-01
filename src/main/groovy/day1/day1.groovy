d41fc8859380d5261d502844938c86e3a4b42756
Viktor Kovacs <crusader8@gmail.com>
2023-12-01 09:14:45 +0100

    day1

--- src/main/groovy/day1/day1.groovy
+++ src/main/groovy/day1/day1.groovy
@@ -1,4 +1,41 @@
 package day1
 
-print("Hello World!")
+def inputFile = new File("../../resources/day1/input")
 
+//part1
+def sum(List<String> lines) {
+    lines.collect {
+        it ->
+            def firstNumber = it.findAll(/\d/)?.first()
+            def lastNumber = it.findAll(/\d/)?.last()
+            def combinedNumber = firstNumber + lastNumber
+            combinedNumber as int
+    }.sum()
+}
+
+println sum(inputFile.readLines())
+
+//part2
+
+def replaceNumber(String line) {
+    def numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    def result = ""
+    for (i in 0..<line.size()) {
+        if (line[i].isNumber()) {
+            result += line[i]
+        } else {
+            for (j in 0..<numbers.size()) {
+                if (line.substring(i).startsWith(numbers[j])) {
+                    result += (j + 1) as String
+                }
+            }
+        }
+    }
+    result
+}
+
+def replacedList = inputFile.readLines().collect {
+    replaceNumber(it)
+}
+
+println sum(replacedList)
