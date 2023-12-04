1c13fdd57d22f2a24b91a9841335ff453a1ccbf1
Viktor Kovacs <crusader8@gmail.com>
2023-12-04 10:03:55 +0100

    day4 part2

--- src/main/groovy/day4/day4.groovy
+++ src/main/groovy/day4/day4.groovy
@@ -3,16 +3,36 @@ package day4
 inputFile = new File("../../resources/day4/input")
 
 //part1
+def winningNumbersCount(def allNumbers) {
+    def winnerNumbers = allNumbers[0].split(" ").findAll { it != "" }
+    def myNumbers = allNumbers[1].split(" ").findAll { it != "" }
+    winnerNumbers.intersect(myNumbers).size() as int
+}
+
 def cardValues = []
+inputFile.readLines().each { line ->
+    def colonSplit = line.split(":")
+    def allNumbers = colonSplit[1].split(" \\| ")
+    def winningNumberCount = winningNumbersCount(allNumbers)
+
+    cardValues << ((winningNumberCount - 1 >= 0 ? Math.pow(2, winningNumberCount - 1) : 0) as int)
+
+
+}
+
+println cardValues.sum() as int
+
+//part2
+Map<Integer, Integer> wonCardMap = [:].withDefault { 1 }
 inputFile.readLines().each { line ->
     def colonSplit = line.split(":")
     def gameId = (colonSplit[0] =~ /Card[ ]+(\d+)/)[0][1] as int
     def allNumbers = colonSplit[1].split(" \\| ")
-    def winnerNumbers = allNumbers[0].split(" ").findAll { it != "" }
-    def myNumbers = allNumbers[1].split(" ").findAll { it != "" }
-    def winningNumberCount = winnerNumbers.intersect(myNumbers).size() as int
-    cardValues << (winningNumberCount - 1 >= 0 ? Math.pow(2, winningNumberCount - 1) : 0)
+    def winningNumbersCount = winningNumbersCount(allNumbers)
+    wonCardMap[gameId] = wonCardMap[gameId] //to make uninitialized map keys set for default value (1)
+    for (i in 0..<winningNumbersCount) {
+        wonCardMap[gameId + (i + 1)] += wonCardMap[gameId]
+    }
 }
 
-println cardValues
-println cardValues.sum() as int
+println wonCardMap.values().sum()
