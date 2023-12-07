a06cd8b516477207134c3755819e087df04d8e01
Viktor Kovacs <crusader8@gmail.com>
2023-12-07 11:27:20 +0100

    day7 part2

--- src/main/groovy/day7/day7.groovy
+++ src/main/groovy/day7/day7.groovy
@@ -7,49 +7,23 @@ import java.util.stream.Collectors
 
 inputFile = new File("../../resources/day7/input")
 
-def handBids = inputFile.readLines().collect {
-    line ->
-        def split = line.split(" ")
-        new HandBid(split[0], split[1] as int)
-}
-
-def sortedHandBids = handBids.sort()
-def sum = 0
-for (i in (1..sortedHandBids.size())) {
-    sum += handBids[i - 1].bid * i
-}
-println sum
-
 @ToString
 class HandBid implements Comparable<HandBid> {
     List<Card> hand
     int bid
+    def typeDeterminator
 
-    HandBid(String hand, int bid) {
-        this.hand = hand.split("").collect { new Card(it) }
+    HandBid(String hand, int bid, def typeDeterminator, def cardValueMapper) {
+        this.hand = hand.split("").collect { new Card(it, cardValueMapper) }
         this.bid = bid
-    }
-
-    Type determineType() {
-        def groupedBy = hand.stream()
-                .collect(Collectors.groupingBy(it -> it, Collectors.counting()))
-
-        def maxValue = groupedBy.values().max()
-
-        if (maxValue == 5) return Type.FIVE_OF_A_KIND
-        if (maxValue == 4) return Type.FOUR_OF_A_KIND
-        if (groupedBy.values().count(3) == 1 && groupedBy.values().count(2) == 1) return Type.FULL_HOUSE
-        if (groupedBy.values().count(3) == 1) return Type.THREE_OF_A_KIND
-        if (groupedBy.values().count(2) == 2) return Type.TWO_PAIR
-        if (groupedBy.values().count(2) == 1) return Type.ONE_PAIR
-        Type.HIGH_CARD
-
+        this.typeDeterminator = typeDeterminator
     }
 
     @Override
     int compareTo(HandBid other) {
-        if (this.determineType().ordinal() < other.determineType().ordinal()) return -1
-        if (this.determineType().ordinal() > other.determineType().ordinal()) return 1
+        typeDeterminator(hand)
+        if (typeDeterminator(hand).ordinal() < typeDeterminator(other.hand).ordinal()) return -1
+        if (typeDeterminator(hand).ordinal() > typeDeterminator(other.hand).ordinal()) return 1
 
         for (i in 0..<5) {
             if (hand[i].value < other.hand[i].value) return -1
@@ -62,18 +36,6 @@ class HandBid implements Comparable<HandBid> {
     String toString() {
         return hand.join()
     }
-
-    enum Type {
-        HIGH_CARD,
-        ONE_PAIR,
-        TWO_PAIR,
-        THREE_OF_A_KIND,
-        FULL_HOUSE,
-        FOUR_OF_A_KIND,
-        FIVE_OF_A_KIND
-    }
-
-
 }
 
 @ToString
@@ -82,14 +44,9 @@ class Card implements Comparable<Card> {
     String cardString
     int value
 
-    Card(String cardString) {
+    Card(String cardString, def cardValueMapper) {
         this.cardString = cardString
-        if (cardString == "A") value = 13
-        if (cardString == "K") value = 12
-        if (cardString == "Q") value = 11
-        if (cardString == "J") value = 10
-        if (cardString == "T") value = 9
-        if (cardString.isNumber()) value = (cardString as int) - 1
+        this.value = cardValueMapper(cardString)
     }
 
     @Override
@@ -101,4 +58,102 @@ class Card implements Comparable<Card> {
     String toString() {
         cardString
     }
+}
+
+def baseType(def groupedBy) {
+    def maxValue = groupedBy.values().max()
+
+    if (maxValue == 5) return Type.FIVE_OF_A_KIND
+    if (maxValue == 4) return Type.FOUR_OF_A_KIND
+    if (groupedBy.values().count(3) == 1 && groupedBy.values().count(2) == 1) return Type.FULL_HOUSE
+    if (groupedBy.values().count(3) == 1) return Type.THREE_OF_A_KIND
+    if (groupedBy.values().count(2) == 2) return Type.TWO_PAIR
+    if (groupedBy.values().count(2) == 1) return Type.ONE_PAIR
+    return Type.HIGH_CARD
+}
+
+//part1
+def typeDeterminator1 = hand -> {
+    def groupedBy = hand.stream()
+            .collect(Collectors.groupingBy(it -> it, Collectors.counting()))
+
+    baseType(groupedBy)
+}
+
+def handBids1 = inputFile.readLines().collect {
+    line ->
+        def split = line.split(" ")
+        new HandBid(split[0], split[1] as int, typeDeterminator1, (cardString) -> {
+            "23456789TJQKA".findIndexOf { it == cardString }
+        })
+}
+
+def part1(originalHandBids) {
+    originalHandBids.collect()
+            .sort()
+            .withIndex().collect { handBid, int i ->
+        handBid.bid * (i + 1)
+    }.sum()
+}
+
+def part1Sum = part1(handBids1)
+assert part1Sum == 252052080
+println part1Sum
+
+//part2
+def typeDeterminator2 = hand -> {
+    def jokerCount = hand.collect { it.cardString }.count { it == "J" }
+    def jokerLessHand = hand.findAll { it -> it.cardString != "J" }
+
+    def groupedBy = jokerLessHand.stream()
+            .collect(Collectors.groupingBy(it -> it, Collectors.counting()))
+
+    def type = baseType(groupedBy)
+
+    jokerCount.times {
+        type = upgrade(type)
+    }
+    type
+}
+
+Type upgrade(Type type) {
+    switch (type) {
+        case Type.HIGH_CARD: return Type.ONE_PAIR
+        case Type.ONE_PAIR: return Type.THREE_OF_A_KIND
+        case Type.TWO_PAIR: return Type.FULL_HOUSE
+        case Type.THREE_OF_A_KIND: return Type.FOUR_OF_A_KIND
+        case Type.FULL_HOUSE: return Type.FOUR_OF_A_KIND
+        case Type.FOUR_OF_A_KIND: return Type.FIVE_OF_A_KIND
+        case Type.FIVE_OF_A_KIND: return Type.FIVE_OF_A_KIND
+    }
+}
+
+def handBids2 = inputFile.readLines().collect {
+    line ->
+        def split = line.split(" ")
+        new HandBid(split[0], split[1] as int, typeDeterminator2, (cardString) -> {
+            "J23456789TQKA".findIndexOf { it == cardString }
+        })
+}
+
+def part2(originalHandBids) {
+    originalHandBids.collect().sort()
+            .withIndex().collect { handBid, int i ->
+        handBid.bid * (i + 1)
+    }.sum()
+}
+
+def sum2 = part2(handBids2)
+assert sum2 == 252898370
+
+println sum2
+
+enum Type {
+    HIGH_CARD,
+    ONE_PAIR,
+    TWO_PAIR,
+    THREE_OF_A_KIND,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    FIVE_OF_A_KIND
 }
